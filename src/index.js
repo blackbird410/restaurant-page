aafe228edae388112e7ef2f626861263e33b20b3
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-01-30 19:51:31 +0800

    Added header and contents

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
+import image1 from './img/clark-douglas.jpg';
+import image2 from './img/nerfee-mirandilla.jpg';
 
 function newHeader() {
 	
@@ -9,11 +11,11 @@ function newHeader() {
 
 	const nav = document.createElement('nav');
 	const btns = [
-		'Hours&Location', 'Menus', 'About', 'Gallery', 'Events', 'Reservations'
+		'Hours&Locations', 'Menus', 'About', 'Gallery', 'Events', 'Reservations'
 		];
 	btns.forEach(btnTitle => {
 		const btn = document.createElement('button');
-		btn.textContent = btnTitle;
+		btn.textContent = btnTitle.toUpperCase();
 		btn.classList.add('nav-btn');
 
 		nav.appendChild(btn);
@@ -27,9 +29,33 @@ function newHeader() {
 
 function content() {
 	const c = document.createElement('div');
-	c.classList.add('content');
-	
+	c.id = 'content';
 
+	const pictureContainer = document.createElement('div');
+	pictureContainer.classList.add('picture-container');
+	
+	[image1, image2].forEach(img => { 
+			const i = document.createElement('img');
+			i.src = img;
+			i.alt = 'A plate of italian food';
+			pictureContainer.appendChild(i);
+
+	});
+	c.appendChild(pictureContainer);
+
+	const descriptionContainer = document.createElement('div');
+	descriptionContainer.classList.add('description-container');
+	const description = document.createElement('p');
+	description.textContent = 'Delicioso is a compilation of lively dining experiences. Delicioso celebrates the best of Italian American dishes with welcoming hospitality and service. Seasonal changes guide the menu of scratch-made pasta made in-house daily, cheeses, antipastis, pizzas and more. Staying true to the Italian dining traditions, Delicioso also offers a "For the Table" dining option curated for a family-style experience, offered nightly alongside the full a la carte menu.';
+	descriptionContainer.appendChild(description);
+
+	const learnMore = document.createElement('button');
+	learnMore.id = 'learn-more-btn';
+	learnMore.textContent = 'LEARN MORE';
+
+	descriptionContainer.appendChild(learnMore);
+
+	c.appendChild(descriptionContainer);
 
 	return c;
 };
